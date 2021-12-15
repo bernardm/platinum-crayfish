09caf29bfec583bfd333431e51dce40ae406097a
denfin <kontakt@dennisfink.de>
2021-12-15 20:30:42 +0100

    Nuxt setup and scaffolding

---
+++ client/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <AppHeader />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import AppHeader from "./../components/layout/AppHeader.vue"
+export default {
+    components: {
+        AppHeader
+    }
+}
+</script>
---
+++ client/pages/decks/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>Decks</h1>
+      <DeckList />
+  </div>
+</template>
+
+<script>
+import DeckList from "./../../components/decks/DeckList.vue"
+export default {
+    components: {
+        DeckList
+    }
+}
+</script>
+
+<style>
+
+</style>
